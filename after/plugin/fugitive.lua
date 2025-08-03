cfe1144b0ba8139bc0f5eb2ae1a859f279a837df
msiesse <msiesse@corefortech.com>
2025-08-03 21:59:41 +0200

    add git mergetool binding

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gl", vim.cmd.Gclog)
 vim.keymap.set("n", "<leader>gd", ":Git difftool<CR>")
+vim.keymap.set("n", "<leader>gm", ":Git mergetool<CR>")
